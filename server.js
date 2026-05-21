3c2afb9b3402567bff26ae51b46bf4b60758de4e
estebanruster <emangascalva@byupathway.edu>
2026-05-21 11:05:07 -0600

    Database connected. Change made in main server.js to the call/start of the server. Now is an async function that test the connection to the database first from the exported variables and functions on the new file db.js. db.js make use of a pool to access the request of multiple queries

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import { fileURLToPath } from 'url';
 import path from 'path';
+import { testConnection } from './src/models/db.js';
+
 
 // Define the the application environment
 const NODE_ENV = process.env.NODE_ENV?.toLowerCase() || 'production';
@@ -50,7 +52,13 @@ app.get('/categories', async (req, res) => {
     res.render('categories', { title });
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is running at http://127.0.0.1:${PORT}`);
-    console.log(`Environment: ${NODE_ENV}`);
+app.listen(PORT, async () => {
+    try {
+        await testConnection();
+        console.log(`Server is running at http://127.0.0.1:${PORT}`);
+        console.log(`Environment: ${NODE_ENV}`);
+    }
+    catch (error) {
+        console.error('Error connecting to the database:', error);
+    }
 });
