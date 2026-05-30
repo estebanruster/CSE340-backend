fed9592435fd92196bf14e5be20b43347af72a30
estebanruster <emangascalva@byupathway.edu>
2026-05-30 15:29:42 -0600

    Project details page made to display the queried project. Links added to go from projects, to project details and also to organization details. Route added to project.ejs. model and controller updated to manage this; and project.ejs view added.

--- src/controllers/projects.js
+++ src/controllers/projects.js
@@ -1,15 +1,25 @@
 // Import any needed model functions
-import { getAllProjects } from '../models/projects.js';
+import { getAllProjects, getUpcomingProjects, getProjectDetails } from '../models/projects.js';
+
+const NUMBER_OF_UPCOMING_PROJECTS = 5;
 
 // Define any controller functions
 const showProjectsPage = async (req, res) => {
     /*Using the import from projects.js; making a call to
     the function that queries all the projects in the db*/
-    const projects = await getAllProjects();
+    const projects = await getUpcomingProjects(NUMBER_OF_UPCOMING_PROJECTS);
 
-    const title = 'Service Projects';
+    const title = 'Upcoming Service Projects';
     res.render('projects', { title, projects });
 };
 
+const showProjectDetailsPage = async (req, res) => {
+    const id = req.params.id;
+    const projectDetails = await getProjectDetails(id);
+
+    const title = 'Project Details';
+    res.render('project', { title, projectDetails });
+};
+
 // Export any controller functions
-export { showProjectsPage };
+export { showProjectsPage, showProjectDetailsPage };
--- src/models/projects.js
+++ src/models/projects.js
@@ -30,6 +30,37 @@ const getProjectsByOrganizationId = async (organizationId) => {
     const result = await db.query(query, queryParams);
 
     return result.rows;
-};
+}
 
-export { getAllProjects, getProjectsByOrganizationId };
+const getUpcomingProjects = async (number_of_projects) => {
+    const query = `
+        SELECT project_id, title, public.project.description, location, TO_CHAR(date, 'YYYY-MM-DD') AS display_date, public.project.organization_id, name
+        FROM public.project
+        JOIN public.organization ON public.project.organization_id = public.organization.organization_id
+        WHERE date >= CURRENT_DATE
+        ORDER BY date ASC
+        LIMIT $1;
+    `;
+
+    const queryParams = [number_of_projects];
+    const result = await db.query(query, queryParams);
+
+    return result.rows;
+}
+
+const getProjectDetails = async (id) => {
+    const query = `
+        SELECT project_id, title, public.project.description, location, TO_CHAR(date, 'YYYY-MM-DD') AS display_date, public.project.organization_id, name
+        FROM public.project
+        JOIN public.organization ON public.project.organization_id = public.organization.organization_id
+        WHERE project_id = $1;
+    `;
+
+    const queryParams = [id];
+    const result = await db.query(query, queryParams);
+
+    // Return the first row of the result set, or null if no rows are found
+    return result.rows.length > 0 ? result.rows[0] : null;
+}
+
+export { getAllProjects, getProjectsByOrganizationId, getUpcomingProjects, getProjectDetails };
